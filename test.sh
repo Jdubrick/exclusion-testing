b2453db4b89ee99d6727e4fbd8e461c64d8c62cd
Jordan Dubrick <jdubrick@redhat.com>
2024-02-05 15:32:40 -0500

    default

---
+++ test.sh
@@ -0,0 +1,38 @@
+tar_files_and_cleanup() {
+  # Find the files to add to the tar archive
+  tarFiles=$(find . \( -not -name 'devfile.yaml' \
+    -a -not -name "meta.yaml" \
+    -a -not -name "*.vsx" \
+    -a -not -name "." \
+    -a -not -name "logo.svg" \
+    -a -not -name "logo.png" \
+    -a -not -name "*.zip" \
+    -a -not -name "OWNERS" \) -maxdepth 1)
+
+  # There are files that need to be pulled into a tar archive
+  if [[ ! -z $tarFiles ]]; then
+    tar -czvf archive.tar $tarFiles > /dev/null
+    rm -rf $tarFiles
+  fi
+} 
+ 
+# Generate the tar archive
+  for stackDir in $outputFolder/stacks/*; do
+  if [[ -d "${stackDir}" ]]; then
+    cd $stackDir
+    if [[ -f "stack.yaml" ]]; then
+      for versionDir in $stackDir/*
+      do
+        if [[ -d "${versionDir}" ]]; then
+          cd $versionDir
+          tar_files_and_cleanup
+        fi
+      done
+    else
+      tar_files_and_cleanup
+    fi
+    cd "$OLDPWD"
+  fi
+  done
+
+
